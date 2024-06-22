ee7d1022a824212c8904d0dc1502b904b1ffa9a8
Ueber-James <ueberjames@gmail.com>
2024-06-22 19:39:43 -0300

    add botao que sobe para o topo da pagina

--- js/script.js
+++ js/script.js
@@ -1,92 +1,90 @@
 let mapa;
-let nomeInput;
-let emailInput;
+let input;
+let email;
 let mensagemInput;
-let nomeMensagem;
-let emailMensagem;
-let mensagemMensagem;
+let mensagemNome;
+let mensagemEmail;
+let mensagem;
 
 document.addEventListener("DOMContentLoaded", function () {
-    mapa = document.getElementById('mapa'); // Inicializa a variável mapa quando o DOM é carregado
-    nomeInput = document.querySelector('#nome');
-    emailInput = document.querySelector('#email');
-    mensagemInput = document.querySelector('#mensagem');
-    nomeMensagem = document.querySelector('#nomeMensagem');
-    emailMensagem = document.querySelector('#emailMensagem');
-    mensagemMensagem = document.querySelector('#mensagemMensagem');
+  mapa = document.getElementById("mapa"); // Inicializa a variável mapa quando o DOM é carregado
+  input = document.querySelector("#nome");
+  email = document.querySelector("#email");
+  mensagemInput = document.querySelector("#mensagem");
+  mensagemNome = document.querySelector("#mensagemNome");
+  mensagemEmail = document.querySelector("#mensagemEmail");
+  mensagem = document.querySelector("#mensagem");
 });
 
 function validarNome() {
-    const nome = nomeInput.value.trim();
+  const nome = input.value.trim();
 
-    if (nome.length < 3) {
-        nomeInput.style.borderColor = 'red';
-        nomeMensagem.textContent = 'O nome deve ter pelo menos 3 letras.';
-    } else {
-        nomeInput.style.borderColor = 'green';
-        nomeMensagem.textContent = '';
-    }
+  if (nome.length < 3) {
+    input.style.borderColor = "red";
+    mensagemNome.textContent = "O nome deve ter pelo menos 3 letras.";
+  } else {
+    input.style.borderColor = "green";
+    mensagemNome.textContent = "";
+  }
 }
 
 function validarEmail() {
-    const email = emailInput.value.trim();
-    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  const email = email.value.trim();
+  const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
-    if (!regex.test(email)) {
-        emailInput.style.borderColor = 'red';
-        emailMensagem.textContent = 'O email deve ser válido.';
-    } else {
-        emailInput.style.borderColor = 'green';
-        emailMensagem.textContent = '';
-    }
+  if (!regex.test(email)) {
+    email.style.borderColor = "red";
+    mensagemEmail.textContent = "O email deve ser válido.";
+  } else {
+    email.style.borderColor = "green";
+    mensagemEmail.textContent = "";
+  }
 }
 
 function validarMensagem() {
-    let mensagem = mensagemInput.value.trim();
+  let mensagem = mensagemInput.value.trim();
 
-    if (mensagem.length > 100) {
-        mensagem = mensagem.slice(0, 100); // Limita a mensagem a 100 caracteres
-        mensagemInput.value = mensagem; // Atualiza o valor do campo
-        mensagemInput.style.borderColor = 'red';
-        mensagemMensagem.textContent = 'A mensagem deve ter no máximo 100 caracteres.';
-    } else {
-        mensagemInput.style.borderColor = 'green';
-        mensagemMensagem.textContent = '';
-    }
+  if (mensagem.length > 100) {
+    mensagem = mensagem.slice(0, 100);
+    mensagemInput.value = mensagem;
+    mensagemInput.style.borderColor = "red";
+    mensagem.textContent = "A mensagem deve ter no máximo 100 caracteres.";
+  } else {
+    mensagemInput.style.borderColor = "green";
+    mensagemMensagem.textContent = "";
+  }
 }
 
 function mapaZoom() {
-    mapa.style.width = '900px';
-    mapa.style.height = '700px';
+  mapa.style.width = "900px";
+  mapa.style.height = "700px";
 }
 
 function mapaNormal() {
-    mapa.style.width = '400px';
-    mapa.style.height = '400px';
+  mapa.style.width = "400px";
+  mapa.style.height = "400px";
 }
-
-
 
 // leva ao topo do site
 
-document.addEventListener('DOMContentLoaded', function() {
-    var backToTopButton = document.getElementById('back-to-top');
+document.addEventListener("DOMContentLoaded", function () {
+  var backToTopButton = document.getElementById("back-to-top");
 
-    // Mostra o botão quando o usuário rolar a página para baixo
-    window.addEventListener('scroll', function() {
-        if (window.scrollY > 300) {
-            backToTopButton.style.display = 'block';
-        } else {
-            backToTopButton.style.display = 'none';
-        }
-    });
+  // Mostra o botão quando o usuário rolar a página para baixo
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 300) {
+      backToTopButton.style.display = "block";
+    } else {
+      backToTopButton.style.display = "none";
+    }
+  });
 
-    // Comportamento de rolagem suave ao clicar no botão
-    backToTopButton.addEventListener('click', function(e) {
-        e.preventDefault();
-        window.scrollTo({
-            top: 0,
-            behavior: 'smooth'
-        });
+  // Comportamento de rolagem suave ao clicar no botão
+  backToTopButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
     });
+  });
 });
